1bb8f7edaecf9dbc0db0cc65b6cd1600b9c6fedb
Álvaro Yuri <alvaro.yuri@outlook.com>
2026-07-24 13:09:34 -0300

    exercícios da aula12 q fiz

---
+++ curso-js/aula12/ex013.js
@@ -0,0 +1,22 @@
+var agora = new Date()
+
+var hora = agora.getHours()
+
+console.log(`Agora são exatamente ${hora} horas`)
+
+
+if (hora >=0 && hora < 5){
+    console.log('Boa Madrugada')
+}
+else if (hora >= 5 && hora < 12){
+    console.log('Bom dia')
+
+}
+else if (hora <=18){
+    console.log('Boa tarde')
+ 
+} else if (hora >18){
+    console.log('Boa noite')
+
+}
+
